577d27dbc685cd1560713518f59bc9c8237c03f9
topofgames <63028208+topofgames@users.noreply.github.com>
2020-04-02 22:13:30 +0100

    Delete home.php

--- css/includes/home.php
+++
@@ -1,8 +0,0 @@
-<div class="index-container">
-	<div class="center">
-
-		<div class="index-title"><?= $settings->title ?></div>
-		<div class="index-text"><?= $language['misc']['index_description'] ?></div>
-
-	</div>
-</div>

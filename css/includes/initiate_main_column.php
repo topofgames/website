809f41ea6a0b0c2625624733853056b3c2bbb531
topofgames <63028208+topofgames@users.noreply.github.com>
2020-04-02 22:13:44 +0100

    Delete initiate_main_column.php

--- css/includes/initiate_main_column.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-	<div class="col-md-<?php echo ($has_sidebar) ? '10' : '12'; ?>">
-
-		<?php if(isset($_GET['page']) && !in_array($_GET['page'], $no_panel_pages)) { ?>
-			<div class="panel panel-default">
-				<div class="panel-body">
-		<?php } ?>
